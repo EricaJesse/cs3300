f75a51715fcdd84eb5e7a9e1515bdf3b6cf51697
Erica Jesse <ejesse@uccs.edu>
2023-04-04 20:39:15 +0000

    books controller with all appropriate basic views added

--- db/migrate/20230404144327_add_fields_to_books.rb
+++
@@ -1,8 +0,0 @@
-class AddFieldsToBooks < ActiveRecord::Migration[6.1]
-  def change
-    add_column :books, :title, :string
-    add_column :books, :plot, :text
-    add_column :books, :rating, :integer
-    add_column :books, :date_completed, :date
-  end
-end
