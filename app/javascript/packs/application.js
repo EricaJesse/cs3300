144871949980b079f06f29b8aa038c7fc79dbc6a
Erica Jesse <ejesse@uccs.edu>
2023-04-13 21:24:30 +0000

    dark mode better, but not great

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,12 +14,17 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-document.addEventListener('DOMContentLoaded', () => {
-    const darkModeToggle = document.getElementById('dark-mode-toggle');
-    const body = document.body;
-  
-    darkModeToggle.addEventListener('click', () => {
-      body.classList.toggle('dark-mode');
+document.addEventListener("DOMContentLoaded", () => {
+  const darkModeToggle = document.getElementById("darkModeToggle");
+  if (darkModeToggle) {
+    darkModeToggle.addEventListener("change", (event) => {
+      if (event.target.checked) {
+        document.body.classList.add("dark-mode");
+      } else {
+        document.body.classList.remove("dark-mode");
+      }
     });
-  });
+  }
+});
+
   
