8ec9dfc6f7e7aeec3076916cc017f665d8187114
Erica Jesse <ejesse@uccs.edu>
2023-04-27 10:14:08 +0000

    testing cont.

--- spec/factories/devise.rb
+++ spec/factories/devise.rb
@@ -1,16 +1,11 @@
 FactoryBot.define do
-    factory :user do
-      id {1}
-      email {"test@user.com"}
-      password {"qwerty"}
-      # Add additional fields as required via your User model
-    end
-  
-    # Not used in this tutorial, but left to show an example of different user types
-    # factory :admin do
-    #   id {2}
-    #   email {"test@admin.com"}
-    #   password {"qwerty"}
-    #   admin {true}
-    # end
-  end
+  # Remove the entire `user` factory block
+
+  # Not used in this tutorial, but left to show an example of different user types
+  # factory :admin do
+  #   id {2}
+  #   email {"test@admin.com"}
+  #   password {"qwerty"}
+  #   admin {true}
+  # end
+end
--- spec/features/books_feature_spec.rb
+++ spec/features/books_feature_spec.rb
@@ -2,31 +2,31 @@
 require "rails_helper"
 
 RSpec.feature "Books", type: :feature do
-  context "Update book" do
-    let(:book) { Book.create(title: "Test title", author: "Test author", plot: "Test plot", rating: 5, date_completed: "2022-01-01") }
-    before(:each) do
-      visit edit_book_path(book)
+  context "Login" do
+    scenario "should sign up" do
+      visit root_path
+      click_link 'Sign up'
+      within("form") do
+        fill_in "Email", with: "testing@test.com"
+        fill_in "Password", with: "123456"
+        fill_in "Password confirmation", with: "123456"
+        click_button "Sign up"
+      end
+      expect(page).to have_content("Welcome! You have signed up successfully.")
     end
 
-    scenario "should be successful" do
-      within("form") do
-        fill_in "Plot", with: "New plot content"
-      end
-      click_button "Update Book"
-      expect(page).to have_content("Book was successfully updated")
-    end
-
-    scenario "should fail when title is blank" do
-      within("form") do
-        fill_in "Title", with: ""
-      end
-      click_button "Update Book"
-      expect(page).to have_content("Title can't be blank")
+    scenario "should log in" do
+      user = FactoryBot.create(:user)
+      login_as(user)
+      visit root_path
+      expect(page).to have_content("Logged in")
     end
   end
 
   context "Create book" do
     before(:each) do
+      user = FactoryBot.create(:user)
+      login_as(user)
       visit new_book_path
     end
 
@@ -42,7 +42,7 @@ RSpec.feature "Books", type: :feature do
       expect(page).to have_content("Book was successfully created")
     end
 
-    scenario "should fail when description is blank" do
+    scenario "should fail when plot is blank" do
       within("form") do
         fill_in "Title", with: "New Book Title"
         fill_in "Author", with: "New Book Author"
@@ -63,6 +63,31 @@ RSpec.feature "Books", type: :feature do
         fill_in "Date completed", with: "2022-02-01"
       end
       click_button "Create Book"
+      expect(page).to have_content("Title can't be blank")
+    end
+  end
+
+  context "Update book" do
+    let(:book) { Book.create(title: "Test title", author: "Test author", plot: "Test plot", rating: 5, date_completed: "2022-01-01") }
+    before(:each) do
+      user = FactoryBot.create(:user)
+      login_as(user)
+      visit edit_book_path(book)
+    end
+
+    scenario "should be successful" do
+      within("form") do
+        fill_in "Plot", with: "New plot content"
+      end
+      click_button "Update Book"
+      expect(page).to have_content("Book was successfully updated")
+    end
+
+    scenario "should fail when title is blank" do
+      within("form") do
+        fill_in "Title", with: ""
+      end
+      click_button "Update Book"
       expect(page).to have_content("Title can't be blank")
     end
   end
