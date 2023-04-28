a31bad77ed2edbb51e45c3b4c1327cf2cb5e056b
Erica Jesse <ejesse@uccs.edu>
2023-04-28 02:29:11 +0000

    finished

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "bootstrap"
-import "../stylesheets/application" // This is your stylesheet file where you will write all the styling code.
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import "bootstrap";
+import "../stylesheets/application"; // This is your stylesheet file where you will write all the styling code.
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
 
 document.addEventListener("DOMContentLoaded", () => {
   const darkModeToggle = document.getElementById("darkModeToggle");
@@ -27,76 +27,47 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
-  
-  document.addEventListener("turbolinks:load", function() {
-    let addCharacter = document.getElementById("add_character");
-    let characterFields = document.querySelector(".nested-fields");
-  
-    addCharacter.addEventListener("click", function(e) {
-      e.preventDefault();
-      let newFields = characterFields.cloneNode(true);
-      let form = document.querySelector("form");
-      form.insertBefore(newFields, addCharacter);
-    });
-  
-    document.addEventListener("click", function(e) {
-      if (e.target && e.target.classList.contains("remove_record")) {
-        e.preventDefault();
-        e.target.parentElement.remove();
-      }
-    });
-  });
-  
-  document.addEventListener("DOMContentLoaded", function() {
-    // Toggle plot visibility
-    const plot = document.getElementById("plot");
-    const togglePlotButton = document.getElementById("togglePlot");
-  
-    if (togglePlotButton) {
-      togglePlotButton.addEventListener("click", function() {
-        plot.classList.toggle("hidden");
-        togglePlotButton.textContent = plot.classList.contains("hidden") ? "Show More" : "Show Less";
-      });
-    }
-  
-    // Toggle character description visibility
-    const toggleCharacterButtons = document.querySelectorAll("[id^='toggleCharacterDescription-']");
-  
-    toggleCharacterButtons.forEach(function(button) {
-      const characterId = button.id.split("-")[1];
-      const characterDescription = document.getElementById(`character-description-${characterId}`);
-  
-      button.addEventListener("click", function() {
-        characterDescription.classList.toggle("hidden");
-        button.textContent = characterDescription.classList.contains("hidden") ? "Show More" : "Show Less";
-      });
-    });
-  });
-  
+document.addEventListener("turbolinks:load", function () {
+  let addCharacter = document.getElementById("add_character");
+  let characterFields = document.querySelector(".nested-fields");
 
-  document.addEventListener("DOMContentLoaded", function() {
-    // Toggle plot visibility
-    const plot = document.getElementById("plot");
-    const togglePlotButton = document.getElementById("togglePlot");
-  
-    if (togglePlotButton) {
-      togglePlotButton.addEventListener("click", function() {
-        plot.classList.toggle("hidden");
-        togglePlotButton.textContent = plot.classList.contains("hidden") ? "Show More" : "Show Less";
-      });
+  addCharacter.addEventListener("click", function (e) {
+    e.preventDefault();
+    let newFields = characterFields.cloneNode(true);
+    let form = document.querySelector("form");
+    form.insertBefore(newFields, addCharacter);
+  });
+
+  document.addEventListener("click", function (e) {
+    if (e.target && e.target.classList.contains("remove_record")) {
+      e.preventDefault();
+      e.target.parentElement.remove();
     }
-  
-    // Toggle character description visibility
-    const toggleCharacterButtons = document.querySelectorAll("[id^='toggleCharacterDescription-']");
-  
-    toggleCharacterButtons.forEach(function(button) {
-      const characterId = button.id.split("-")[1];
-      const characterDescription = document.getElementById(`character-description-${characterId}`);
-  
-      button.addEventListener("click", function() {
-        characterDescription.classList.toggle("hidden");
-        button.textContent = characterDescription.classList.contains("hidden") ? "Show More" : "Show Less";
-      });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Toggle plot visibility
+  const plot = document.getElementById("plot");
+  const togglePlotButton = document.getElementById("togglePlot");
+
+  if (togglePlotButton) {
+    togglePlotButton.addEventListener("click", function () {
+      plot.classList.toggle("hidden");
+      togglePlotButton.textContent = plot.classList.contains("hidden") ? "Show More" : "Show Less";
+    });
+  }
+
+  // Toggle character description visibility
+  const toggleCharacterButtons = document.querySelectorAll("[id^='toggleCharacterDescription-']");
+
+  toggleCharacterButtons.forEach(function (button) {
+    const characterId = button.id.split("-")[1];
+    const characterDescription = document.getElementById(`character-description-${characterId}`);
+
+    button.addEventListener("click", function () {
+      characterDescription.classList.toggle("hidden");
+      button.textContent = characterDescription.classList.contains("hidden") ? "Show More" : "Show Less";
     });
   });
-  
+});
